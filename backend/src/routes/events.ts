2442893d0a732a7dbbbe1a3b6c2e0cf43f522d8d
pfreitasbarbosa <contato.pfmb@gmail.com>
2020-10-26 15:39:47 -0300

    Add docs for event presence confirmation endpoint
    Also moves route logic to EventPresenceController.

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import ensureAuthenticationMiddleware from '../middlewares/ensureAuthentication';
 import EventsController from '../controllers/EventsController';
 import EventSubscriptionController from '../controllers/EventSubscriptionController';
-import ConfirmPresenceService from '../services/ConfirmPresenceService';
+import EventPresenceController from '../controllers/EventPresenceController';
 
 const eventsController = new EventsController();
 const eventSubscriptionController = new EventSubscriptionController();
+const eventPresenceController = new EventPresenceController();
 
 const eventsRouter = Router();
 eventsRouter.use(ensureAuthenticationMiddleware);
@@ -14,26 +15,6 @@ eventsRouter.get('/', eventsController.index);
 eventsRouter.get('/:id', eventsController.show);
 eventsRouter.post('/subscribe/:id', eventSubscriptionController.store);
 eventsRouter.post('/unsubscribe/:id', eventSubscriptionController.remove);
-eventsRouter.post('/presence/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-
-    const parsedEventId = Number(id);
-    if (Number.isNaN(parsedEventId)) {
-      throw new Error('Event id must be an integer number');
-    }
-
-    const confirmPresenceService = new ConfirmPresenceService();
-
-    const confirmedEvent = await confirmPresenceService.execute({
-      eventId: parsedEventId,
-      userId: request.user.id,
-    });
-
-    return response.status(201).json(confirmedEvent);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+eventsRouter.post('/presence/:id', eventPresenceController.store);
 
 export default eventsRouter;
